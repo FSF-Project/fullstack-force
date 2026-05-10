c65f88b16a90d4719f5e8508d6a7fb8aa04af4a3
HUBI971 <76213310+HUBI971@users.noreply.github.com>
2026-05-10 19:49:26 +0200

    Add files via upload

---
+++ backend/API/BookStore/Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿namespace BookStore.Models;
+
+public class Cart
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public int Ilosc { get; set; }
+
+    public Product? Product { get; set; }
+}
---
+++ backend/API/BookStore/Models/OrderItem.cs
@@ -0,0 +1,14 @@
+﻿namespace BookStore.Models;
+
+public class OrderItem
+{
+    public int Id { get; set; }
+
+    public int OrderId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public int Ilosc { get; set; }
+
+    public decimal Cena { get; set; }
+}
