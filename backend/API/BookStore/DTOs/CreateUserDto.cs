fc47e5cd66e62ba7cc22df2daedc01ca73ffcc45
HUBI971 <76213310+HUBI971@users.noreply.github.com>
2026-05-10 19:48:01 +0200

    Add files via upload

---
+++ backend/API/BookStore/DTOs/CreateUserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace BookStore.DTOs;
+
+public class CreateUserDto
+{
+    public string Imie { get; set; } = string.Empty;
+
+    public string Nazwisko { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string Haslo { get; set; } = string.Empty;
+}
